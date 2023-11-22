59049b39839d8f7f1c0fe968b37ff2f39df0ae82
konfle <konrad.flejter@gmail.com>
2023-11-22 14:10:39 -0500

    add comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+# Wiedza ekspertów na temat języków programowania w zależności od warunków aplikacji
 knowledge_base = {
     'Rule1': {'condition': 'app_type == "web"', 'language': ["Python", "JavaScript", "Java", "Ruby", "PHP", "Go"]},
     'Rule2': {'condition': 'app_type == "mobile" and performance', 'language': ["Java", "Kotlin", "Swift"]},
@@ -8,15 +9,19 @@ knowledge_base = {
     'Rule5': {'condition': 'app_type == "text" and not performance', 'language': ["Bash/Shell", "Python", "Perl", "Lua", "Go"]},
 }
 
+# Funkcja wnioskująca język programowania w zależności od warunków aplikacji
 def infer(app_conditions):
     for rule, data in knowledge_base.items():
         condition = data['condition']
         language = data['language']
 
+        # Sprawdzenie, czy warunek reguły jest spełniony
         if eval(condition, globals(), app_conditions):
+            # Losowy wybór języka z dostępnych
             return language[random.randint(0, len(language)-1)]
-    return None
+    return "Coś poszło nie tak..."
 
+# Funkcja pobierająca dane od użytkownika
 def get_user_input():
     app_type = input("Podaj typ aplikacji: ").strip().lower()
     performance = input("Wydajnosc (y/n): ").strip().lower()
@@ -27,6 +32,7 @@ def get_user_input():
     return {'app_type': app_type, "performance": performance}
 
 if __name__ == "__main__":
+    # Pobranie danych od użytkownika, podjęcie decyzji i wydrukowanie wyniku
     user_input = get_user_input()
     decision = infer(user_input)
     print("Decyzja: {}".format(decision))
