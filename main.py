683f1541b78dc82b192236fc24c9d3013f5debbe
konfle <konrad.flejter@gmail.com>
2023-11-26 12:12:37 -0500

    moving sensitive data to env variables

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import random
 import psycopg2
+import os
+
 from psycopg2 import sql
+from dotenv import load_dotenv
+
+
+# Wczytaj zmienne środowiskowe z pliku .env
+load_dotenv()
+
+# Uzyskaj wartości zmiennych środowiskowych
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Funkcja pobierająca wiedzę ekspercką z bazy danych
 def get_rules_from_database():
     conn = psycopg2.connect(
-        host="localhost",
-        database="isw",
-        user="osboxes",
-        password="ahe123"
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
     cursor = conn.cursor()
     cursor.execute('SELECT condition, languages FROM knowledge_base')
