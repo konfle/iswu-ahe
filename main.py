355465c1a36664dd7616f44e9dc25099f5868978
konfle <konrad.flejter@gmail.com>
2023-11-26 10:16:20 -0500

    migrate knowledge base to the psql db

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import random
+import psycopg2
+from psycopg2 import sql
 
-# Wiedza ekspertów na temat języków programowania w zależności od warunków aplikacji
-knowledge_base = {
-    'Rule1': {'condition': 'app_type == "web"', 'language': ["Python", "JavaScript", "Java", "Ruby", "PHP", "Go"]},
-    'Rule2': {'condition': 'app_type == "mobile" and performance', 'language': ["Java", "Kotlin", "Swift"]},
-    'Rule3': {'condition': 'app_type == "mobile" and not performance', 'language': ["Java", "Kotlin", "Swift", "Objective-C", "Flutter", "React Native"]},
-    'Rule4': {'condition': 'app_type == "text"', 'language': ["Bash/Shell", "Go"]},
-    'Rule5': {'condition': 'app_type == "text" and not performance', 'language': ["Bash/Shell", "Python", "Perl", "Lua", "Go"]},
-}
+def get_rules_from_database():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="isw",
+        user="osboxes",
+        password="ahe123"
+    )
+    cursor = conn.cursor()
+    cursor.execute('SELECT condition, languages FROM knowledge_base')
+    rules = cursor.fetchall()
+    conn.close()
+    return rules
 
-# Funkcja wnioskująca język programowania w zależności od warunków aplikacji
-def infer(app_conditions):
-    for rule, data in knowledge_base.items():
-        condition = data['condition']
-        language = data['language']
-
-        # Sprawdzenie, czy warunek reguły jest spełniony
+def infer(app_conditions, rules):
+    for condition, language in rules:
         if eval(condition, globals(), app_conditions):
             # Losowy wybór języka z dostępnych
             return language[random.randint(0, len(language)-1)]
     return "Coś poszło nie tak..."
-
+    
 # Funkcja pobierająca dane od użytkownika
 def get_user_input():
     app_type = input("Podaj typ aplikacji: ").strip().lower()
@@ -32,7 +33,10 @@ def get_user_input():
     return {'app_type': app_type, "performance": performance}
 
 if __name__ == "__main__":
+    # Pobierz reguły wiedzy ekspertów z bazy danych PostgreSQL
+    rules_from_db = get_rules_from_database()
+
     # Pobranie danych od użytkownika, podjęcie decyzji i wydrukowanie wyniku
     user_input = get_user_input()
-    decision = infer(user_input)
+    decision = infer(user_input, rules_from_db)
     print("Decyzja: {}".format(decision))
