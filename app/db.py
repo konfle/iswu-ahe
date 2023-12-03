464168650b5a81e89bf1125ca6507be5e8033196
konfle <konrad.flejter@gmail.com>
2023-12-03 18:37:50 +0100

    update func for fetch data from db

--- app/db.py
+++ app/db.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-
 from dotenv import load_dotenv
 
 knowledge_base = [
@@ -22,13 +21,19 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Funkcja pobierająca wiedzę ekspercką z bazy danych
-def get_rules_from_database():
+def get_rules_from_database(
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+):
     conn = psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password
     )
     cursor = conn.cursor()
     cursor.execute('SELECT condition, languages FROM knowledge_base')
