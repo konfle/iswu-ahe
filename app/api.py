b3aee910a99ce540d2cc7d36e005242858555abd
konfle <konrad.flejter@gmail.com>
2023-12-04 20:24:21 +0100

    add router tags and title, description and contact to app instance

--- app/api.py
+++ app/api.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI, HTTPException, APIRouter
 import app.inference as ai
 import app.db as ad
 
-app = FastAPI()
+app = FastAPI(title="Application System for Selection of Optimal Programming Language",
+              description="An inference system that, using prior information about the IT project and the user's "
+                          "goals, helps select the most appropriate programming language for the task at hand.",
+              contact={"name": "konfle", "url": "https://github.com/konfle"})
 router = APIRouter()
 
 
-@router.get(path="/user_input", tags=["Collection of user data"])
+@router.get(path="/user_input", tags=["Collection of User Data"])
 def get_user_input(app_type: str, performance: bool):
     rules_from_db = ad.get_rules_from_database()
     user_input = {"app_type": app_type, "performance": performance}
