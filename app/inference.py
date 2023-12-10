a20db183662ad5f57bf1f937db41678587bfee92
konfle <konrad.flejter@gmail.com>
2023-12-10 21:14:02 +0100

    Clean up after project split

--- app/inference.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# Funkcja wnioskująca język programowania w zależności od warunków aplikacji
-def infer(app_conditions, rules):
-    for condition, language in rules:
-        if eval(condition, globals(), app_conditions):
-            # Losowy wybór języka z dostępnych
-            return language[random.randint(0, len(language)-1)]
-    return None
